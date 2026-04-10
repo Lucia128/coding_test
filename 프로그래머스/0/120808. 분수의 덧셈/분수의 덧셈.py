8e8067c845ec57af3851e0f0b20f0003eb256da7
Lucia <steadybloom.dev@gmail.com>
2026-04-10 22:58:11 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 9.28 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,16 @@
+import math
+
+def solution(numer1, denom1, numer2, denom2):
+    answer = []
+    
+    a = (numer1*denom2 + numer2*denom1)
+    b = (denom1*denom2)
+    
+    gcd = math.gcd(a, b)
+    a = a // gcd
+    b = b // gcd
+    
+    answer.append(a)
+    answer.append(b)
+    
+    return answer
